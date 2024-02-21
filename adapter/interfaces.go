0e5ee5b64b6ab872228a09b28058cefc806441ae
Ha Nguyen <ha.nguyenk07@gmail.com>
2024-02-21 17:01:19 +0700

    feat: add methods to adapter - SubscribeNewHead - FilterLogs - BlockNumber - HeaderByHash - HeaderByNumber

--- adapter/interfaces.go
+++ adapter/interfaces.go
@@ -13,4 +13,11 @@ import (
 type EthClientAdapter interface {
 	CallContract(ctx context.Context, msg types.CallMsg, blockNumber *big.Int) ([]byte, error)
 	CallContractAtHash(ctx context.Context, msg types.CallMsg, blockHash common.Hash) ([]byte, error)
+
+	SubscribeNewHead(ctx context.Context, headerChannel chan<- *types.Header) (types.Subscription, error)
+	FilterLogs(ctx context.Context, query types.FilterQuery) ([]types.Log, error)
+
+	BlockNumber(ctx context.Context) (uint64, error)
+	HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error)
+	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
 }
